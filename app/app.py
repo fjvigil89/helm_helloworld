095791c943736633f14185c2f8aae965e486336e
Frank Josué Vigil Vega <frankjosue.vigilvega@gmail.com>
2024-02-22 13:16:20 +0100

    transformando path de chats

--- app/app.py
+++ app/app.py
@@ -8,7 +8,5 @@ def hello_world():
     return 'Hello, World!'
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0',port=5000, debug=True)
 
-#if __name__ == '__main__':
-#	app.run(host="0.0.0.0", port=80, debug=True)
